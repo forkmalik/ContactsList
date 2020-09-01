3a10233b59bd3d796bce6c87df7e1c0e946c3940
Vlad Savchenko <polkamaraka@gmail.com>
2020-09-01 18:24:10 +0300

    remove done

---
+++ src/components/store/store.js
@@ -0,0 +1,12 @@
+let store = {
+  contacts: [
+    {
+        id: 0,
+        name: "Ivan Ivanov",
+        phone: "6487646",
+        email: "ivan@mail.com"
+    }
+  ]
+};
+
+export default store;
