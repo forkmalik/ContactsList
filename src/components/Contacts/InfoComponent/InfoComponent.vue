1c431a4f5606c8b7d5cae22e5ae6e83997f7d08a
Vlad Savchenko <polkamaraka@gmail.com>
2020-09-10 16:21:29 +0300

    start removing func

--- src/components/Contacts/InfoComponent/InfoComponent.vue
+++ src/components/Contacts/InfoComponent/InfoComponent.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="info-container">
-    <p class="info-item">{{contactPropsName}}: {{info}}</p>
-  </div>
+  <ol class="info-container">
+    <li class="info-item">
+      <a>{{contactPropsName}}: {{info}}</a>
+      <button class="contact-list__btn edit-btn">Edit</button>
+      <button @click="removeField(contactPropsName)" class="contact-list__btn">X</button>
+    </li>
+  </ol>
 </template>
 
 <script>
@@ -10,15 +14,33 @@ export default {
   props: ["info", "contact"],
   data() {
     return {
+      contact: this.contact,
       contactPropsName: Object.keys(this.contact).find(
         (key) => this.contact[key] === this.info
       ),
     };
   },
+  methods: {
+    removeField: function (contactPropsName) {
+      let currentPropsName = this.contactPropsName;
+      if (currentPropsName == contactPropsName) {
+        let contactKeys = Object.keys(this.contact);
+        let index = contactKeys.indexOf(currentPropsName);
+        let contactInfoArr = Object.entries(this.contact);
+        contactInfoArr.splice(index, 1);
+        this.contact = Object.fromEntries(contactInfoArr)
+        console.log(this.contact);
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+.info-container {
+  list-style-type: none;
+  padding: 0;
+}
 .info-item {
   position: relative;
   font-family: "Trebuchet MS", "Lucida Sans";
@@ -32,5 +54,30 @@ export default {
   font-size: 20px;
   letter-spacing: 2px;
   transition: 0.3s all linear;
+  &:hover {
+    border-right: 10px solid green;
+    cursor: pointer;
+  }
+  button {
+    position: absolute;
+    right: 20px;
+    top: 0;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .contact-list__btn {
+    border: none;
+    margin-top: 10px;
+    border-radius: 20px;
+    background-color: #f05d22;
+    color: white;
+  }
+  .edit-btn {
+    width: 100px;
+    height: 20px;
+    font-size: 16px;
+    margin-right: 50px;
+  }
 }
 </style>
